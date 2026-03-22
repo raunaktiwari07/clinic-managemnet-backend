1201365cff0f0512c2bdd7a92889a841b3bd21ed
raunaktiwari07 <raunakt950@gmail.com>
2026-03-22 11:27:30 +0530

    modify

--- src/apis/comman-apis/routes.js
+++ src/apis/comman-apis/routes.js
@@ -1,26 +1,26 @@
-const express = require("express");
-const router = express.Router();
-//const{}=require("./validation");
-const{sendOTP,verifyOTP,resetPassword}= require("./controller");
-const responsehandler = require("../../utils/responseHandler.js")
-const{protect}=require("../../middlewares")
+// const express = require("express");
+// const router = express.Router();
+// //const{}=require("./validation");
+// const{sendOTP,verifyOTP,resetPassword}= require("./controller");
+// const responsehandler = require("../../utils/responseHandler.js")
+// const{protect}=require("../../middlewares")
 
-router.post(
-    "/send-otp",
+// router.post(
+//     "/send-otp",
  
-    responsehandler(sendOTP)
-)
+//     responsehandler(sendOTP)
+// )
 
-router.post(
-    "/verify-otp",
-    protect,
-    responsehandler(verifyOTP)
-)
+// router.post(
+//     "/verify-otp",
+//     protect,
+//     responsehandler(verifyOTP)
+// )
 
-router.post(
-    "/reset-password",
-    protect,
-    responsehandler(resetPassword)
-)
+// router.post(
+//     "/reset-password",
+//     protect,
+//     responsehandler(resetPassword)
+// )
 
-module.exports = router;
+// module.exports = router;
